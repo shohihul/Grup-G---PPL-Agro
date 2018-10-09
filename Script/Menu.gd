f208ffb20e0498897ded7b0913253b426effbff8
shohihul <shohihul29maksud@gmail.com>
2018-10-09 23:33:22 +0700

    modul 1

--- Script/Menu.gd
+++ Script/Menu.gd
@@ -1,41 +1,26 @@
 extends Node2D
 var popupMulai
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var dataDihapus
 
 func _ready():
 	popupMulai = get_node("popupMulai")
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
 
 func _on_mulai_pressed():
 	popupMulai.show()
-	pass # replace with function body
 
-
-func _on_tidak_pressed():
+func _on_tidak_pressed(): #tombol tidak popup
 	popupMulai.hide()
-	pass # replace with function body
 
-
-func _on_ya_pressed():
+func _on_ya_pressed(): #tombol ya popup
 	get_tree().change_scene('res://Scene/gameplay.tscn')
-	pass # replace with function body
+
+func _on_lanjutkan_pressed(): #tombol lanjutkan
+	get_tree().change_scene('res://Scene/gameplay.tscn') 
+
+func _on_keluar_pressed(): #tombol quit
+	get_tree().quit() 
 
 
-func _on_lanjutkan_pressed():
-	popupMulai.show()
-	pass # replace with function body
+func _on_petunjuk_pressed(): #tombol petunjuk
+	get_tree().change_scene('res://Scene/petunjuk.tscn') 
 
-
-func _on_keluar_pressed():
-	get_tree().quit()
-	pass # replace with function body
